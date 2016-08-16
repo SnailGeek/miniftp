9930ccb7e9e83ad7338d79a2243f6dd5a48dcbc7
SnailGeek <CodingSnail@qq.com>
2016-08-16 15:08:30 +0800

    miniftp01

---
+++ miniftp01/common.h
@@ -0,0 +1,30 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pwd.h>
+
+
+#define ERR_EXIT(m)\
+do\
+{\
+	perror(m);\
+	exit(EXIT_FAILURE);\
+}while(0)
+
+#define MAX_COMMAND_LINE 1024
+#define MAX_COMMAND 32
+#define MAX_ARG 1024
+
+#endif
