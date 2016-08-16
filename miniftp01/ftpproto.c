b725a2725b058fc6cc2a88e2d4b4178a0c314dbe
SnailGeek <CodingSnail@qq.com>
2016-08-16 21:19:42 +0800

    modify ftpproto.c

--- miniftp01/ftpproto.c
+++ miniftp01/ftpproto.c
@@ -1,16 +1,30 @@
 #include "ftpproto.h"
 #include "sysutil.h"
+#include "str.h"
 
 void handle_child(session_t *sess)
 {
 	writen(sess->ctrl_fd, "220 (miniftpd 0.1)\r\n", strlen("220 (miniftpd 0.1)\r\n"));
+	int ret;
 	while(1){
 		memset(sess->cmdline, 0, sizeof(sess->cmdline));
 		memset(sess->cmd, 0, sizeof(sess->cmd));
 		memset(sess->arg, 0, sizeof(sess->arg));
-		readline(sess->ctrl_fd, sess->cmdline, MAX_COMMAND_LINE);
+		ret = readline(sess->ctrl_fd, sess->cmdline, MAX_COMMAND_LINE);
+		if(ret == -1)
+			ERR_EXIT("readline");
+		else if(ret == 0)
+			exit(EXIT_SUCCESS);
+		printf("cmdline=[%s]\n", sess->cmdline);
 
+		//去除\r\n	
+		str_strim_crlf(sess->cmdline);
+		printf("cmdline=[%s]\n", sess->cmdline);
 		//解析FTP命令和参数
+		str_split(sess->cmdline, sess->cmd, sess->arg, ' ');
+		printf("cmd=[%s] arg=[%s]\n", sess->cmd, sess->arg);
+		//将命令转化为大写
+		str_upper(sess->cmd);
 		//处理FTP命令
 	}
 
